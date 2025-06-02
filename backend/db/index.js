5845d9bc96ff4a1cda98d0d5969a465baf569c9d
elijahwebdesigner <elijahb187@nycstudents.net>
2025-06-02 11:19:02 -0400

    index.js (with coments)

--- backend/db/index.js
+++ backend/db/index.js
@@ -3,14 +3,19 @@ const fs = require("fs");
 const path = require("path");
 require("dotenv").config();
 
+//i belive this is a code to bring files to this program 
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
+//this is to read the files of whatever is brung 
+
 const schemaPath = path.join(__dirname, "schema.sql");
 const schema = fs.readFileSync(schemaPath, "utf8");
 
+//i dont know what schema is but i think its to read the files? 
 (async () => {
   try {
     await pool.query(schema);
@@ -18,6 +23,8 @@ const schema = fs.readFileSync(schemaPath, "utf8");
   } catch (err) {
     console.error("❌ Failed to run schema.sql:", err);
   }
+  //this sends a error code if someone cant accsess it 
 })();
 
 module.exports = pool;
+//exports it?
